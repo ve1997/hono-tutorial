6e0ec3179c521e0471718f4f96f599ac3c5f19d2
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-06-04 14:09:51 +0900

    add example

--- app/example/server/page.tsx
+++ app/example/server/page.tsx
@@ -1,9 +1,19 @@
-export default function Page() {
+import { client } from "@/app/lib/hono";
+export default async function Page() {
+	const res = await client.api.hello.$get({
+		query: {
+			name: "User",
+		},
+	});
+	const data = await res.json();
 	return (
 		<div className="m-4 flex min-w-[400px] flex-col items-center justify-center rounded bg-white p-4 shadow-md">
 			<h1 className="mb-4 font-bold text-2xl text-gray-800">Hello, Hono!</h1>
 			<p className="text-gray-700 text-lg">
 				Welcome to Hono! This is an example page.
+			</p>
+			<p className="text-gray-700 text-lg">
+				API Response: <strong>{data.message}</strong>
 			</p>
 		</div>
 	);
