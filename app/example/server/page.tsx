008abc97ec6ae167f63ec8e4965107b724bff91f
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-06-04 14:06:55 +0900

    add example/server/page

---
+++ app/example/server/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+	return (
+		<div className="m-4 flex min-w-[400px] flex-col items-center justify-center rounded bg-white p-4 shadow-md">
+			<h1 className="mb-4 font-bold text-2xl text-gray-800">Hello, Hono!</h1>
+			<p className="text-gray-700 text-lg">
+				Welcome to Hono! This is an example page.
+			</p>
+		</div>
+	);
+}
